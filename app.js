582af612ef59b33ef21f1302d910c54b6612ce02
Tom Smith <tom.smith@forter.com>
2022-07-30 21:17:50 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const nunjucks = require('nunjucks')
+
+/*************************************************/
+
+// let config = require('./config.json')
+
+const ip_addresses = {
+	APPROVE: '0.0.0.1',
+	DECLINE: '0.0.0.2',
+	VERIFICATION_REQUIRED: '0.0.0.4'
+}
+
+/*************************************************/
+
+const app = express()
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended: true}))
+
+app.use(express.static('public'))
+
+nunjucks
+    .configure('views', {
+        autoescape: true,
+        express: app,
+        watch: true
+    })
+
+/*************************************************/
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+	console.log(`app listening at http://localhost:${port}`)
+})
+
+/*************************************************/
+
+app.get('/favicon.ico', (req, res) => {
+	res.sendStatus(200)
+	return
+})
+
+app.get('/', (req, res) => {
+
+	obj = {
+		FORTER_EU: process.env.FORTER_EU,
+		FORTER_SITE_ID: process.env.FORTER_SITE_ID
+	}
+
+	res.render ('index.html', obj)
+})
