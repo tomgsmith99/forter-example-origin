a085f9cb6d3ebd8acd93d3595d10aa07d8094700
Tom Smith <tomgsmith99@gmail.com>
2022-09-20 10:57:23 -0400

    fix guy pearce

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use(express.urlencoded({extended: true}))
+// app.use(express.urlencoded({extended: true}))
 
 nunjucks
     .configure('views', {
