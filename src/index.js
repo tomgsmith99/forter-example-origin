5569257cda90a11c185ab34cf929d3d91bd763e2
Tom Smith <tomgsmith99@gmail.com>
2022-10-04 14:45:11 -0400

    add envs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 
 // this function allows for testing various Forter responses
 async function get_ip(username, ip_address) {
@@ -29,92 +20,102 @@ async function get_ip(username, ip_address) {
   
 async function handleRequest(request, env) {
 
-	const reqBody = await request.json()
-  
-	// Pull the username (and password in prod) from the request body
-	const { username } = reqBody
+	if (request.method === 'POST') {
 
-	const config = {
-		body: JSON.stringify({ username: username }),
-		headers: {
-			'Content-type': 'application/json; charset=UTF-8'
-		},
-		method: 'post'
-	}
+		const reqBody = await request.json()
+	
+		// Pull the username (and password in prod) from the request body
+		const { username } = reqBody
 
-	// check the user's credentials against the user directory
-	// const modifiedRequest = new Request(url, config)
+		const config = {
+			body: JSON.stringify({ username: username }),
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8'
+			},
+			method: 'post'
+		}
 
-	const modifiedRequest = new Request(env.USER_DIRECTORY_URL, config)
+		// check the user's credentials against the user directory
+		// const modifiedRequest = new Request(url, config)
 
-	const credsResponse = await fetch(modifiedRequest)
-  
-	// if the user directory says the credentials are good
-	// then call Forter, else send response (usually 401) to client 
-	if (credsResponse.status == 200) {
-  
-	  const user_id = "e520-ba9a-367-60b" // this value should come from the user directory
-  
-	  const ip_address = await get_ip(username, request.headers.get('CF-Connecting-IP'))
-  
-	  var myHeaders = new Headers()
-	  myHeaders.append("api-version", env.FORTER_API_VERSION)
-	  myHeaders.append("Content-Type", "application/json")
-	  myHeaders.append("Authorization", "Basic " + env.FORTER_KEY_ENCODED)
-  
-	  var raw = JSON.stringify({
-		"accountId": user_id,
-		"connectionInformation": {
-		  "customerIP": ip_address,
-		  "userAgent": request.headers.get('user-agent')
-		},
-		"loginMethodType": "PASSWORD",
-		"loginStatus": "SUCCESS",
-		"eventTime": Date.now()
-	  })
-  
-	  var requestOptions = {
-		method: 'POST',
-		headers: myHeaders,
-		body: raw,
-		redirect: 'follow'
-	  }
-  
-	  const forter_url = 'https://' + env.FORTER_SITE_ID + '.api.forter-secure.com/v2/accounts/login/' + user_id
-  
-	  const forter_response = await fetch(forter_url, requestOptions)
-  
-	  const forter_response_obj = await forter_response.json()
-  
-	  console.log("the forter decision is: " + JSON.stringify(forter_response_obj))
-  
-	  if (forter_response_obj.forterDecision == "DECLINE") {
-		return new Response('', { status: 401 })
-	  }
-	  else {
-		return new Response(JSON.stringify(forter_response_obj), { 
-		  status: 200,
-		  headers: {
-			'content-type': 'application/json',
-		  }
-		})
-	  }
+		const modifiedRequest = new Request(env.USER_DIRECTORY_URL, config)
+
+		const credsResponse = await fetch(modifiedRequest)
+	
+		// if the user directory says the credentials are good
+		// then call Forter, else send response (usually 401) to client 
+		if (credsResponse.status == 200) {
+	
+			const user_id = "e520-ba9a-367-60b" // this value should come from the user directory
+		
+			const ip_address = await get_ip(username, request.headers.get('CF-Connecting-IP'))
+		
+			var myHeaders = new Headers()
+			myHeaders.append("api-version", env.FORTER_API_VERSION)
+			myHeaders.append("Content-Type", "application/json")
+			myHeaders.append("Authorization", "Basic " + env.FORTER_KEY_ENCODED)
+		
+			var raw = JSON.stringify({
+				"accountId": user_id,
+				"connectionInformation": {
+				"customerIP": ip_address,
+				"userAgent": request.headers.get('user-agent')
+				},
+				"loginMethodType": "PASSWORD",
+				"loginStatus": "SUCCESS",
+				"eventTime": Date.now()
+			})
+		
+			var requestOptions = {
+				method: 'POST',
+				headers: myHeaders,
+				body: raw,
+				redirect: 'follow'
+			}
+		
+			const forter_url = 'https://' + env.FORTER_SITE_ID + '.api.forter-secure.com/v2/accounts/login/' + user_id
+		
+			const forter_response = await fetch(forter_url, requestOptions)
+		
+			const forter_response_obj = await forter_response.json()
+		
+			console.log("the forter decision is: " + JSON.stringify(forter_response_obj))
+	
+			if (forter_response_obj.forterDecision == "DECLINE") {
+				return new Response('', { status: 401 })
+			}
+			else {
+				return new Response(JSON.stringify(forter_response_obj), { 
+					status: 200,
+					headers: {
+						'content-type': 'application/json',
+					}
+				})
+			}
+		}
+		else {
+			return new Response(credsResponse.body, {
+				status: credsResponse.status
+			})
+		}
 	}
 	else {
-	  return new Response(credsResponse.body, {
-		status: credsResponse.status
-	  })    
+		const init = {
+			headers: {
+			  'content-type': 'text/html;charset=UTF-8',
+			},
+		};
+
+		const response = await fetch(env.ORIGIN, init);
+		const r = await response.text()
+		return new Response(r, init);
 	}
 }
 
 export default {
 	async fetch(request, env) {
 
-		if (request.method === 'POST') {
-			return handleRequest(request, env);
-		}
-		else {
-			return new Response("The request was not a post.");
-		}
+		return handleRequest(request, env);
+
 	},
 };
